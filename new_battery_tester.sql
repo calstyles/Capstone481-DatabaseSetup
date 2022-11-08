98407c2c99cdf9982bf4919849567c0e98546282
calstyles <78227433+calstyles@users.noreply.github.com>
2022-11-08 01:31:42 -0500

    Update new_battery_tester.sql

--- new_battery_tester.sql
+++ new_battery_tester.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS battery_percentages;
 DROP TABLE IF EXISTS cart_location;
 
 CREATE TABLE battery_information(
+	cart VARCHAR(30),
 	battery_voltage DECIMAL(6,3),
 	battery_current DECIMAL(6,3),
 	battery_power DECIMAL(6,3),
@@ -10,12 +11,14 @@ CREATE TABLE battery_information(
 );
 
 CREATE TABLE battery_percentages(
+	cart VARCHAR(30),
 	battery_state_of_charge DECIMAL(6,3),
 	battery_depth_of_discharge DECIMAL(6,3),
 	battery_health NUMERIC(5, 2)
 );
 
 CREATE TABLE cart_location(
+	cart VARCHAR(30),
 	cart_geo_location VARCHAR(30),
 	cart_longitude decimal(10,7),
 	cart_latitude decimal(10,7)
@@ -24,3 +27,7 @@ CREATE TABLE cart_location(
 SELECT * FROM battery_information;
 SELECT * FROM battery_percentages;
 SELECT * FROM cart_location;
+
+INSERT VALUES INTO battery_information(
+
+);
