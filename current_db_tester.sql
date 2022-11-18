8afed31b6b701d63cdee795a7b3fe1a5facf5a4c
calstyles <78227433+calstyles@users.noreply.github.com>
2022-11-18 15:26:27 -0500

    Update current_db_tester.sql

--- current_db_tester.sql
+++ current_db_tester.sql
@@ -4,9 +4,7 @@ CREATE TABLE battery_measurements(
 -- 	cart VARCHAR(30),
 	battery_voltage DECIMAL(6,3),
 	battery_current DECIMAL(6,3),
--- 	battery_resistance DECIMAL(6,3),
 	battery_state_of_charge DECIMAL(6,3)
--- 	battery_watt_hours DECIMAL(6,3)
 );
 
 
