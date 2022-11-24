e814670fdb8bc9af33793ece609a48b7037f9455
calstyles <78227433+calstyles@users.noreply.github.com>
2022-11-23 22:01:34 -0500

    Update current_db_tester.sql

--- current_db_tester.sql
+++ current_db_tester.sql
@@ -2,41 +2,15 @@
 DROP TABLE IF EXISTS battery_measurements;
 DROP TABLE IF EXISTS cart_location;
 
+--This is the table that will gather the battery measurements
 CREATE TABLE battery_measurements(
 	battery_voltage DECIMAL(6,3),
 	battery_current DECIMAL(6,3),
 	battery_state_of_charge DECIMAL(6,3)
 );
 
+--This is the table that will gather the geo-location of the cart
 CREATE TABLE cart_location(
 	cart_longitude decimal(21,14),
 	cart_latitude decimal(21,14)
 );
-
---Table Queries
-SELECT * FROM battery_measurements;
-SELECT * FROM cart_location;
-
---Voltage, Volts
-SELECT battery_voltage
-FROM battery_measurements;
-
---Current, Amps
-SELECT battery_current
-FROM battery_measurements;
-
---Resistance, Ohms
-SELECT battery_voltage/battery_current AS "battery_resistance"
-FROM battery_measurements;
-
---Power, Watts
-SELECT battery_voltage * battery_current AS "battery_power"
-FROM battery_measurements;
-
---State of Charge
-SELECT battery_state_of_charge
-FROM battery_measurements;
-
---Depth of Discharge
-SELECT 100 - battery_state_of_charge AS "battery_depth_of_discharge"
-FROM battery_measurements;
