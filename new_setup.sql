3cb58ccfab347e2723c468b2eadefc199ab73670
Caleb Styles <78227433+calstyles@users.noreply.github.com>
2023-01-24 19:35:42 -0500

    Add files via upload

--- new_setup.sql
+++ new_setup.sql
@@ -5,25 +5,25 @@ DROP TABLE IF EXISTS cart_location CASCADE;
 
 --This is the table that has all of our cart information
 CREATE TABLE carts(
-	cartID INT PRIMARY KEY,
+	id INT PRIMARY KEY,
 	groupID INT
 );
 
 --This is the table that will gather the battery measurements
 CREATE TABLE battery_measurements(
-	cartID INT,
+	id INT,
 	battery_voltage DECIMAL(6,3),
 	battery_current DECIMAL(6,3),
 	battery_state_of_charge DECIMAL(6,3),
-	PRIMARY KEY (cartID),
-	FOREIGN KEY (cartID) REFERENCES carts(cartID)
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES carts(id)
 );
 
 --This is the table that will gather the geo-location of the cart
 CREATE TABLE cart_location(
-	cartID INT,
+	id INT,
 	cart_longitude decimal(21,14),
 	cart_latitude decimal(21,14),
-	PRIMARY KEY (cartID),
-	FOREIGN KEY (cartID) REFERENCES carts(cartID)
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES carts(id)
 );
