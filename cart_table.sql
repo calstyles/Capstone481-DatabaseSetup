1c838e003041d9e3eaf5392ff94b547d133cccb5
Caleb Styles <78227433+calstyles@users.noreply.github.com>
2023-02-12 23:12:50 -0500

    Update cart_table.sql

--- cart_table.sql
+++ cart_table.sql
@@ -1,22 +1,8 @@
 --Drops all tables if they exist, otherwise these lines of code are ignored; USE IF YOU ARE RESETTING THE battery_measurements AND geo-location TABLES!
-DROP TABLE IF EXISTS battery_measurements CASCADE;
-DROP TABLE IF EXISTS cart_location CASCADE;
+DROP TABLE IF EXISTS carts CASCADE;
 
---This is the table that will gather the battery measurements
-CREATE TABLE battery_measurements(
-	cartID INT,
-	timestamp BIGINT,
-	battery_voltage DECIMAL(6,3),
-	battery_current DECIMAL(6,3),
-	battery_state_of_charge DECIMAL(6,3),
-	FOREIGN KEY (cartID) REFERENCES carts(id)
-);
-
---This is the table that will gather the geo-location of the cart
-CREATE TABLE cart_location(
-	cartID INT,
-	timestamp BIGINT,
-	cart_longitude decimal(21,14),
-	cart_latitude decimal(21,14),
-	FOREIGN KEY (cartID) REFERENCES carts(id)
+--This is the table that has all of our cart information
+CREATE TABLE carts(
+	id bigserial PRIMARY KEY,
+	cartID INT
 );
