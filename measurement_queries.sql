934dc081634a754a40af53d17eed8de2ac7a9374
Caleb Styles <78227433+calstyles@users.noreply.github.com>
2023-03-20 23:29:49 -0400

    Ordered All Queries by Most Recent Timestamp
    The queries are all listed by the most recent timestamp to make reading the data easier to understand what information is coming from the current cart.

--- measurement_queries.sql
+++ measurement_queries.sql
@@ -9,55 +9,66 @@ SELECT * FROM cart_location;
 --Voltage, Volts
 SELECT battery_voltage
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Current, Amps
 SELECT battery_current
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Resistance, Ohms
 SELECT battery_voltage/battery_current AS "battery_resistance"
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Power, Watts
 SELECT battery_voltage * battery_current AS "battery_power"
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --State of Charge
 SELECT battery_state_of_charge
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Depth of Discharge
 SELECT 100 - battery_state_of_charge AS "battery_depth_of_discharge"
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Geo-location, Longitude
 SELECT cart_longitude 
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Geo-location, Latitude
 SELECT cart_latitude 
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Altitude
 SELECT altitude
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Speed
 SELECT speed
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
 
 --Date Inserted
 SELECT DATE('1970-01-01') + (timestamp * INTERVAL '1 millisecond') 
 AS time_inserted
 FROM battery_measurements
-WHERE cartid = 1;
+WHERE cartid = 1
+ORDER BY timestamp DESC;
